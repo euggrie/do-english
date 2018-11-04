92fea0e5de41147dc363c5fd6bd592a4c45c3d2d
eugenia <megrieff@gmail.com>
2018-11-04 20:51:07 +0000

    Add empty pages.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
-  get :hello_world, to: 'pages#hello_world'
+  get :about_me, to: 'pages#about_me'
+  get :videos, to: 'pages#videos'
+  get :contact_me, to: 'pages#contact_me'
 end
